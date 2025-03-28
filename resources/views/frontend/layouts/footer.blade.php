c5857abe11c5632febb8f19044ac2192b51e0b38
Rajan Sharma <rajankumarsharma681@gmail.com>
2025-03-28 11:39:53 +0530

    add to cart open in center

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -105,7 +105,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="addToCartModal" tabindex="-1" aria-labelledby="addToCartModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="addToCartModalLabel">Select Size & Color</h5>
